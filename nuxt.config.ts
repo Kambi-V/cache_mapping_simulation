ff82d8081ecfbcea3a3909e63a966c42c0ec04b7
Kambi-V <erratickambi@gmail.com>
2023-10-14 20:14:32 +0300

    feat(tailwind): add tailwind.config.js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,15 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "nuxt-gtag"],
   gtag: {
-    id: 'G-RXG9XVPSMH'
+    id: "G-RXG9XVPSMH",
   },
   css: [
+    "~/assets/css/tailwind.css",
     "primevue/resources/themes/lara-light-blue/theme.css",
     "primevue/resources/primevue.css",
     "primeicons/primeicons.css",
   ],
   build: {
-    transpile: ["primevue","vee-validate/dist/rules"],
+    transpile: ["primevue", "vee-validate/dist/rules"],
   },
 });
-
-
